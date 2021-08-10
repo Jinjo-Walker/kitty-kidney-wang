0bb5597f2dea921ed7b92b3484262ece9bf2e323
zhaohongyang <610267732@qq.com>
2021-08-10 08:44:16 +0800

    user.js api

--- backEnd/wjxy/routes/user.js
+++ backEnd/wjxy/routes/user.js
@@ -6,6 +6,42 @@ const SmsClient = tencentcloud.sms.v20210111.Client;
 //创建路由
 const r = express.Router();
 
+function send(num) {
+	return new Promise((reslove, reject) => {
+		const clientConfig = {
+			credential: {
+				secretId: "AKIDObZ0FFzUkkU2MRGrCH6fy2sSntkPq2zi",
+				secretKey: "XpsfCVZNwHNDn8E5h3A0bw2tWt1uXv4y",
+			},
+			region: "ap-nanjing",
+			profile: {
+				httpProfile: {
+					endpoint: "sms.tencentcloudapi.com",
+				},
+			},
+		};
+		let yzm = Math.floor(Math.random() * 10000);
+		const client = new SmsClient(clientConfig);
+		const params = {
+			"PhoneNumberSet": [num],
+			"SmsSdkAppId": "1400558463",
+			"SignName": "华梦超作品分享",
+			"TemplateId": "1070489",
+			"TemplateParamSet": [
+				`${yzm}`
+			]
+		};
+		client.SendSms(params).then(
+			(data) => {
+				reslove({ data: data, yzm: yzm });
+			},
+			(err) => {
+				console.error("error", err);
+			}
+		);
+	});
+}
+
 //req.body解析中间件
 r.use(express.urlencoded({
 	extended: false
@@ -22,7 +58,7 @@ r.use((err, req, res, next) => {
 
 //注册请求
 r.post("/reg", (req, res, next) => {
-	var sql = 'insert into table_user(id,account,password,phone,user_name) values(?,?,?,?,?)';
+	var sql = 'insert into table_user(id,account,password,phone,user_name) values(null,?,?,?,?)';
 	pool.query(sql, [req.body.account, req.body.password, req.body.phone, req.body.user_name], function (err, result) {
 		if (err) {
 			next(err);
@@ -88,7 +124,7 @@ r.get("/phone_exist", (req, res, next) => {
 
 //账号登录请求
 r.post("/account_login", (req, res, next) => {
-	var sql = 'select * from table_user where (account = ? and password = ?) and isDel = 0';
+	var sql = 'select id,account,phone,user_name from table_user where (account = ? and password = ?) and isDel = 0';
 	pool.query(sql, [req.body.account, req.body.password], function (err, result) {
 		if (err) {
 			next(err);
@@ -96,35 +132,107 @@ r.post("/account_login", (req, res, next) => {
 		}
 		if (result.length > 0) {
 			res.send({
-				code: 200
+				code: 200,
+				message: '登录成功',
+				result: result
 			});
 		} else {
 			res.send({
-				code: 201
+				code: 201,
+				message: '登录失败'
 			});
 		}
 	});
 });
 
+//短信验证接口
+r.get("/yzm", (req, res, next) => {
+	if (err) {
+		next(err);
+		return;
+	}
+	send(req.query.phone).then(result => {
+		if (result.data.SendStatusSet[0].Code == 'Ok') {
+			res.send({
+				code: 200,
+				message: '验证成功',
+				yzm:result.yzm
+			});
+		}
+		else{
+			res.send({
+				code: 201,
+				message: '验证失败'
+			});
+		}
+	});
+
+});
+
 //手机登录请求
 r.post("/phone_login", (req, res, next) => {
-	var sql = 'select * from table_user where (phone = ?) and isDel = 0';
+	var sql = 'select id,account,phone,user_name from table_user where (phone = ?) and isDel = 0';
 	pool.query(sql, [req.body.phone], function (err, result) {
 		if (err) {
 			next(err);
 			return;
 		}
 		if (result.length > 0) {
+			//短信验证接口判断
 			res.send({
-				code: 200
+				code: 200,
+				message: '登录成功',
+				result: result
 			});
 		} else {
-			res.send({
-				code: 201
+			var sql2 = 'insert into table_user(id,account,password,phone,user_name) values(null,?,?,?,null)';
+			pool.query(sql2, [req.body.phone, '123456', req.body.phone], function (err2, result2) {
+				if (err2) {
+					next(err2);
+					return;
+				}
+				if (result2.affectedRows > 0) {
+					//短信验证接口判断
+					res2.send({
+						code: 200,
+						message: '新用户注册登录成功，初始密码为123456，请尽快修改密码。',
+						result: result2
+					});
+				} else {
+					res.send({
+						code: 201,
+						message: '注册登录失败'
+					});
+				}
 			});
 		}
 	});
 });
+
+//账号信息查询请求
+r.post("/account_info", (req, res, next) => {
+	var sql = 'select id,account,phone,user_name from table_user where id = ?';
+	pool.query(sql, [req.body.id], function (err, result) {
+		if (err) {
+			next(err);
+			return;
+		}
+		if (result.length > 0) {
+			res.send({
+				code: 200,
+				message: '查询成功',
+				result: result
+			});
+		} else {
+			res.send({
+				code: 201,
+				message: '查询失败'
+			});
+		}
+	});
+});
+
+//账户优惠卷查询
 
 //注销请求
 r.get("/logout", (req, res) => {
